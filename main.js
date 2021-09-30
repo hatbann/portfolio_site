b42bb195c1a5f1fbaa47245ed1e40c2446a1dcf2
hatbann <86299528+hatbann@users.noreply.github.com>
2021-09-30 17:17:52 +0900

    navbar menu click

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+});
