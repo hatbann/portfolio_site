30a7f9cee7fd12a206768177a9f2bcba2f4af88e
hatbann <86299528+hatbann@users.noreply.github.com>
2021-09-30 17:38:31 +0900

    JS home transparent

--- main.js
+++ main.js
@@ -17,7 +17,19 @@ contactMeBtn.addEventListener('click', () =>{
     scrollIntoView('#Contact');
 })
 
+//Transparent Home when scrolling
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll' , ()=>
+{
+    home.style.opacity = 1-window.scrollY/ homeHeight;
+
+})
+
+
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior:"smooth"});
 }
+
+
