54c7687d063ca04b896c20a3717cae9e270d3e65
hatbann <86299528+hatbann@users.noreply.github.com>
2021-10-03 11:01:30 +0900

    remove active from nave
    change active -> selected

--- main.js
+++ main.js
@@ -54,10 +54,18 @@ const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
 workBtnContainer.addEventListener('click', (e)=>{
     const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    console.log(filter);
     if(filter == null)
     {
         return;
     }
+
+    //Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+
     projectContainer.classList.add('anim-out');
     setTimeout(()=>{
         projects.forEach((project) => {
